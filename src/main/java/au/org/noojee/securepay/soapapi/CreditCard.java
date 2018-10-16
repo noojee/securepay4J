99db49a60c9ec0849d5e6d901a330e629c989609
Brett Sutton <bsutton@noojee.com.au>
2018-10-16 17:19:25 +1100

    cleaned find bug warnings.

--- src/main/java/au/org/noojee/securepay/soapapi/CreditCard.java
+++ src/main/java/au/org/noojee/securepay/soapapi/CreditCard.java
@@ -2,7 +2,6 @@ package au.org.noojee.securepay.soapapi;
 
 import java.security.SecureRandom;
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.Random;
 
 import org.apache.logging.log4j.util.Strings;
